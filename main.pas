372c5bcc3f29189f53144b70eacc090890cf6f93
Hevanafa <dhevansafa4@gmail.com>
2025-06-24 16:02:37 +0700

    moved driver code to main.pas

---
+++ main.pas
@@ -0,0 +1,64 @@
+unit Main;
+
+interface
+
+procedure Posit92;
+
+implementation
+
+uses SDL2;
+
+var
+  window: PSDL_Window;
+  renderer: PSDL_Renderer;
+  keyboardState: PUInt8;
+
+  done: boolean;
+  { More of your game state here }
+
+procedure update;
+begin
+  SDL_PumpEvents;
+
+  { Your update code here }
+
+  if keyboardState[SDL_SCANCODE_ESCAPE] = 1 then
+    done := true;
+end;
+
+procedure draw;
+begin
+  SDL_SetRenderDrawColor(renderer, $64, $95, $ED, SDL_ALPHA_OPAQUE);
+  SDL_RenderClear(renderer);
+
+  { Your drawing code here }
+
+  SDL_RenderPresent(renderer);
+end;
+
+procedure Posit92;
+begin
+  if SDL_Init(SDL_INIT_VIDEO) < 0 then halt(1);
+
+  window := SDL_CreateWindow('Hello SDL', 100, 100, 320, 200, SDL_WINDOW_SHOWN);
+  if window = nil then halt(1);
+
+  renderer := SDL_CreateRenderer(window, 1, 0);
+  if renderer = nil then halt(1);
+
+  SDL_SetHint(SDL_HINT_RENDER_SCALE_QUALITY, 'nearest');
+
+  keyboardState := SDL_GetKeyboardState(nil);
+
+  done := false;
+  repeat
+    update;
+    draw;
+    SDL_Delay(16);
+  until done;
+
+  SDL_Quit
+
+end;
+
+end.
