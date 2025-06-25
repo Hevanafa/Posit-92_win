67ae2e56cceb9ca0028739134f401f17c8697dda
Hevanafa <dhevansafa4@gmail.com>
2025-06-25 14:00:04 +0700

    added mouse unit interface

--- units/mouse.pas
+++ units/mouse.pas
@@ -2,8 +2,48 @@ unit Mouse;
 
 interface
 
+uses
+  Bitmap;
+
+var
+  cursor: PImage;
+  mouseX, mouseY: word;
+  mouseButton: byte;
+
+procedure loadCursor;
+procedure freeCursor;
+procedure initMouse;
+procedure updateMouse;
+procedure debugMouse;
+procedure drawMouse;
+procedure resetMouse;
+
 
 implementation
+
+uses Events;
+
+var
+  mouseInitialised: boolean;
+
+procedure loadCursor;
+begin
+  loadImage(cursor, 'CURSOR.PNG')
+end;
+
+procedure initMouse;
+begin
+  { Originally used to init the mouse driver }
+  loadCursor;
+  mouseInitialised := true;
+end;
+
+procedure updateMouse;
+begin
+  mouseX := sdlEvent.motion.x;
+  mouseY := sdlEvent.motion.y;
+  mouseButton := sdlEvent.button.button;
+end;
 
 end.
 
