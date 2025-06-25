4251b5028815235d356282f120d09c92393df4ad
Hevanafa <dhevansafa4@gmail.com>
2025-06-25 14:03:03 +0700

    added the rest of mouse implementations

--- units/mouse.pas
+++ units/mouse.pas
@@ -21,7 +21,8 @@ procedure resetMouse;
 
 implementation
 
-uses Events;
+uses
+  Conv, Events, VGA;
 
 var
   mouseInitialised: boolean;
@@ -31,9 +32,14 @@ begin
   loadImage(cursor, 'CURSOR.PNG')
 end;
 
+procedure freeCursor;
+begin
+  freeImage(cursor)
+end;
+
 procedure initMouse;
 begin
-  { Originally used to init the mouse driver }
+  { Originally used to init the mouse driver in DOS }
   loadCursor;
   mouseInitialised := true;
 end;
@@ -43,6 +49,29 @@ begin
   mouseX := sdlEvent.motion.x;
   mouseY := sdlEvent.motion.y;
   mouseButton := sdlEvent.button.button;
+end;
+
+procedure debugMouse;
+begin
+  if not mouseInitialised then begin
+    print('Mouse has not been initialised!', 0, 0, 15);
+    exit
+  end;
+
+  print('Mouse: {x:' + i32str(mouseX) + ', y:' + i32str(mouseY) + '}', 0, 0, 15);
+  print('Buttons: ' + i32str(mouseButton), 0, 8, 15);
+end;
+
+procedure drawMouse;
+begin
+  if not mouseInitialised then exit;
+
+  spr(cursor, mouseX, mouseY)
+end;
+
+procedure resetMouse;
+begin
+  { Originally used to reset the mouse driver in DOS }
 end;
 
 end.
